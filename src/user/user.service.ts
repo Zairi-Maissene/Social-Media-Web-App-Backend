2ad3e454df4bcf0c3ac4cb0e1b6fd5234a69c97c
NESSMA <nessma.hamidou@insat.u-carthage.tn>
2023-05-25 19:01:46 +0100

    user authentification

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,22 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import {ConflictException, Injectable} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { FriendRequest } from '../friend-request/entities/friend-request.entity';
-import { CreateUserDto } from './dto/create-user.dto';
+import { SubscribeUser} from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(FriendRequest)
-    private friendRequestEntityRepository: Repository<FriendRequest>,
-    @InjectRepository(User) private userEntityRepository: Repository<User>,
+      @InjectRepository(User)
+           private userRepository: Repository<User>
   ) {}
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
+
 
   findAll() {
     return `This action returns all user`;
@@ -34,10 +32,29 @@ export class UserService {
     return `This action removes a #${id} user`;
   }
   async getAllRequests(userId: string) {
-    const user = await this.userEntityRepository.findOne({
+    const user = await this.userRepository.findOne({
       where: { id: userId },
       relations: ['friends'],
     });
     return user.friends;
+  }
+  async getALLUsers():Promise<User[]> {
+    return await this.userRepository.find();
+  }
+  async subscribe(userInfo: SubscribeUser): Promise<User>
+  {
+
+    const user = this.userRepository.create({...userInfo})
+    user.salt = await bcrypt.genSalt();
+    user.password = await bcrypt.hash(user.password,user.salt)
+    try {
+      await this.userRepository.save(user);
+    }
+    catch (err) {
+      throw new ConflictException("username and email doivent etre uniques")
+    }
+    delete user.password;
+    delete user.salt;
+    return user ;
   }
 }
