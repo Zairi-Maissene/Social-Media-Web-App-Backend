99efc1fe69d906babfca60cd348ecd528dcfa2a4
NESSMA <nessma.hamidou@insat.u-carthage.tn>
2023-05-25 19:01:46 +0100

    user authentification

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { FriendRequest } from './friend-request/entities/friend-request.entity';
 import { UserModule } from './user/user.module';
 import { DbSeederModule } from './db-seeder/db-seeder.module';
 import { FriendRequestModule } from './friend-request/friend-request.module';
+import { AuthentificationModule } from './authentification/authentification.module';
 
 import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
@@ -26,7 +27,8 @@ import { CommentModule } from './comment/comment.module';
     FriendRequestModule,
     PostModule,
     CommentModule,
-    
+
+    AuthentificationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
