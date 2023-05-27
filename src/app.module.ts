ee7e1500c542a8ad74c11a7bde38db5f94fc883f
NESSMA <nessma.hamidou@insat.u-carthage.tn>
2023-05-27 12:31:02 +0100

    user sign in

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { FriendRequest } from './friend-request/entities/friend-request.entity';
 import { UserModule } from './user/user.module';
 import { DbSeederModule } from './db-seeder/db-seeder.module';
 import { FriendRequestModule } from './friend-request/friend-request.module';
-import { AuthentificationModule } from './authentification/authentification.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
@@ -22,7 +21,6 @@ import { AuthentificationModule } from './authentification/authentification.modu
     UserModule,
     DbSeederModule,
     FriendRequestModule,
-    AuthentificationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
