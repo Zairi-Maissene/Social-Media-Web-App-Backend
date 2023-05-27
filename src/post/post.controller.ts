7404a24ee0d1a812bdc447852883251882cad064
NESSMA <nessma.hamidou@insat.u-carthage.tn>
2023-05-27 15:00:00 +0100

    ass USER decorator

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,25 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import {Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, UseGuards, Req} from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { UploadedFile, UseInterceptors } from '@nestjs/common/decorators';
 import { FileInterceptor } from '@nestjs/platform-express/';
-
-
+import {JwtAuthGuard} from "../user/Guards/jwt-auth.guard";
+import { User } from '../decorators/user.decorator';
+import {Request} from 'express';
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @Post('add')
   @UseInterceptors(FileInterceptor('file'))
-  async create(@Body('content') content: string, @UploadedFile() file: Express.Multer.File,) {
-    console.log("content :" +content);
-   const image = file.originalname;
-   return await  this.postService.addPost(content,image);
- 
+  @UseGuards(JwtAuthGuard)
+  async create(@Body() body :{ content: string,image:string},  @UploadedFile() file: Express.Multer.File,@User() user){
+
+   console.log("content :" +body.content);
+   //const image = file.originalname;
+    console.log('image',body.image);
+
+    const post:CreatePostDto ={"content":body.content, "imageUrl":body.image,"owner":user}
+   return await  this.postService.addPost(post);
+
+
   }
   
   @Patch('update/:id')
+  @UseGuards(JwtAuthGuard)
   @UseInterceptors(FileInterceptor('file'))
   async update(@Param('id') id: string, 
   @UploadedFile() file: Express.Multer.File,
@@ -29,8 +37,9 @@ export class PostController {
   }
 
   @Get()
-  findAll() {
-    return this.postService.findAll();
+  @UseGuards(JwtAuthGuard)
+  findAll(@Req() request:Request) {
+    return request.user;
   }
   @Get('likes/:id')
   getLikesById(@Param ('id')id: number) {
