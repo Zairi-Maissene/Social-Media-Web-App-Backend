40965ab96af72074ac23e6c37b0c5e4d4423d924
Chiraz_Doss <you@example.com>
2023-06-03 05:22:44 +0100

    modifying

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
@@ -32,11 +41,6 @@ export class CommentController {
     return this.commentService.findAll();
   }
 
-  /*@Get('byWriter')
-  @UseGuards(JwtAuthGuard)
-  findByWriter(@User() user) {
-    return this.commentService.getCommentsByWriter(user.id);
-  }*/
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.commentService.findOne(id);
